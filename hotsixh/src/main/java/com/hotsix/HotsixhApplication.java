64cd52f7e9a57bc0cc540bdfd8de3a2dcb13611e
logeu <logeu@localhost>
2024-05-07 18:12:26 +0900

    a

--- hotsixh/src/main/java/com/hotsix/HotsixhApplication.java
+++ hotsixh/src/main/java/com/hotsix/HotsixhApplication.java
@@ -2,8 +2,9 @@ package com.hotsix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class HotsixhApplication {
 
 	public static void main(String[] args) {
